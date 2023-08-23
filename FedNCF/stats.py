7cda4f140634697a8e96da15d59f9567388f6005
NNHieu <ngochieutb13@gmail.com>
2023-08-23 08:43:21 +0000

    add wandb logger

--- FedNCF/stats.py
+++ FedNCF/stats.py
@@ -3,6 +3,10 @@ import numpy as np
 import scipy as sp
 import torch
 
+from typing import Any, Dict
+
+from omegaconf import OmegaConf
+
 class TimeStats(object):
     def __init__(self) -> None:
         self.reset()
@@ -39,3 +43,48 @@ def count_num_important_component(A: torch.Tensor, percents=[0.99, 0.95, 0.9]):
     S = S / S.sum()
     S = np.cumsum(S)
     return [np.sum(S < p) + 1 for p in percents], S
+
+
+
+def log_hyperparameters(object_dict: Dict[str, Any]) -> None:
+    """Controls which config parts are saved by Lightning loggers.
+
+    Additionally saves:
+        - Number of model parameters
+
+    :param object_dict: A dictionary containing the following objects:
+        - `"cfg"`: A DictConfig object containing the main config.
+        - `"model"`: The Lightning model.
+        - `"trainer"`: The Lightning trainer.
+    """
+    hparams = {}
+
+    cfg = OmegaConf.to_container(object_dict["cfg"])
+    model = object_dict["model"]
+
+    hparams["model"] = cfg["net"]
+
+    # save number of model parameters
+    hparams["model/params/total"] = sum(p.numel() for p in model.parameters())
+    hparams["model/params/trainable"] = sum(
+        p.numel() for p in model.parameters() if p.requires_grad
+    )
+    hparams["model/params/non_trainable"] = sum(
+        p.numel() for p in model.parameters() if not p.requires_grad
+    )
+
+    hparams["data"] = cfg["DATA"]
+    hparams["dataloader"] = cfg["DATALOADER"]
+    hparams["fed"] = cfg["FED"]
+    hparams["trainer"] = cfg["TRAIN"]
+
+    # hparams["callbacks"] = cfg.get("callbacks")
+    hparams["exp"] = cfg.get("EXP")
+    hparams["eval"] = cfg.get("EVAL")
+
+    hparams["task_name"] = cfg.get("task_name")
+    # hparams["tags"] = cfg.get("tags")
+    # hparams["ckpt_path"] = cfg.get("ckpt_path")
+    hparams["seed"] = cfg.get("seed")
+
+    return hparams
